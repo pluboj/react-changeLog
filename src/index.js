2e79023d5e71a781a1a24c07ef43aa61adc37837
plubojacky <plubojacky@email.com>
2017-08-04 12:31:24 -0700

    Set up API

--- src/index.js
+++ src/index.js
@@ -4,29 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const data = [
-    {
-      'time':'2 minutes ago',
-      'member':'Jerry',
-      'desc':'Created new account'
-    },
-    {
-      'time':'12 minutes ago',
-      'member':'Linda',
-      'desc':'Added comment'
-    },
-    {
-      'time':'1 hour ago',
-      'member':'Mike',
-      'desc':'Created new account'
-    },
-    {
-      'time':'2 hours ago',
-      'member':'Jenna',
-      'desc':'Updated title'
-    },
-    ];
-
 
 ReactDOM.render(
 	<App 
